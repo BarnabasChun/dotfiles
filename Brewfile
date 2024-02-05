394a06ad9003757ab2f7856dd7f7faf178ee5fb9
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-02-05 12:38:38 -0500

    Switch to Brewfile for installation

---
+++ Brewfile
@@ -0,0 +1,20 @@
+# Taps
+tap "homebrew/bundle"
+tap "homebrew/cask-fonts"
+
+# Brews (Packages)
+brew "starship"
+brew "trash"
+
+# Casks (Applications)
+cask "alfred"
+cask "font-fira-code-nerd-font"
+cask "iterm2"
+# cask "google-chrome"
+# cask "visual-studio-code"
+
+# VS Code Extensions
+vscode "akamud.vscode-theme-onedark"
+vscode "eamodio.gitlens"
+vscode "PKief.material-icon-theme"
+vscode "tamasfe.even-better-toml"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -5,13 +5,4 @@ echo "\n<<< Starting Homebrew setup >>>\n"
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# terminal related
-brew install --cask --no-quarantine iterm2
-brew tap homebrew/cask-fonts && brew install --cask font-fira-code-nerd-font
-brew install starship
-
-# misc
-brew install trash
-# brew install --cask --no-quarantine visual-studio-code
-# brew install --cask --no-quarantine google-chrome
-brew install --cask --no-quarantine alfred
+brew bundle --verbose
