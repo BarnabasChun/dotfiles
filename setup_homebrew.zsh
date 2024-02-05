bbccbb92bb65a0ecede727624f42c22571fd36a8
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-02-05 16:33:23 -0500

    Use exists() to prevent unnecessary Homebrew installs

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,8 +1,11 @@
-# !/usr/bin/env zsh
+#!/usr/bin/env zsh
 
+if exists brew; then
+  echo "Skipping install of homebrew. It is already installed."
+else
+  echo "Installing homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 echo "\n<<< Starting Homebrew setup >>>\n"
-
-# install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle --verbose
