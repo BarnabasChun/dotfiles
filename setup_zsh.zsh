38be084dcce6d2ad603c5e86d17f3829c62d227e
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-02-06 11:53:20 -0500

    Add conditionals to prevent redundant setup actions

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,8 +2,16 @@
 
 echo "\n<<< Starting ZSH setup >>>\n"
 
-echo "Enter supeuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if grep "homebrew" /etc/shells > /dev/null; then
+  echo "Homebrew zsh already acceptable shell"
+else
+  echo "Enter supeuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+if echo $SHELL | grep "homebrew" > /dev/null; then
+  echo "Homebrew zsh already login shell"
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
