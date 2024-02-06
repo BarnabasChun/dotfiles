6ce60594aedb15bc1d4bfbe8a4cb6eb03aea6222
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-02-06 11:08:55 -0500

    Add zsh setup
    Add Homebrew-installed zsh to /etc/shells and change login shell.

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,11 +1,12 @@
 #!/usr/bin/env zsh
 
+echo "\n<<< Starting Homebrew setup >>>\n"
+
 if exists brew; then
   echo "Skipping install of homebrew. It is already installed."
 else
   echo "Installing homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-echo "\n<<< Starting Homebrew setup >>>\n"
 
 brew bundle --verbose
