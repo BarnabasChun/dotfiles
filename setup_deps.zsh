16905b77a073f47b08e8eb4ad81ef5fb3deb0f45
Barnabas Chun <28585999+BarnabasChun@users.noreply.github.com>
2024-03-26 13:26:23 -0400

    Add if checks for installing asdf & node plugin

--- setup_deps.zsh
+++ setup_deps.zsh
@@ -1,8 +1,23 @@
 #!/usr/bin/env zsh
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install nodejs latest
+if [ -d "${HOME}/.asdf" ]
+then
+  echo "asdf already downloaded"
+else 
+  echo "downloading asdf"
+  # https://asdf-vm.com/guide/getting-started.html#_2-download-asdf
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+fi
+
+NODE_VERSIONS=$(asdf list nodejs)
+
+if [ "${?}" -ne 0 ]
+then
+  echo "installing nodejs"
+  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf install nodejs latest
+fi
+
 asdf global nodejs $(asdf list nodejs | tail -1 | sed 's/^ *//g')
 source ~/.zshrc
 
